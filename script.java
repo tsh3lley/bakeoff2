9b194af338295edc6c4d5cd8e8377437d3bfcc06
the kid <thomasjshelley@gmail.com>
2017-03-26 14:28:23 -0400

    first commit

---
+++ script.java
@@ -0,0 +1,328 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+int index = 0;
+
+//your input code should modify these!!
+float screenTransX = 0;
+float screenTransY = 0;
+float screenRotation = 0;
+float screenZ = 50f;
+
+int trialCount = 8; //this will be set higher for the bakeoff
+float border = 0; //have some padding from the sides
+int trialIndex = 0; //what trial are we on
+int errorCount = 0;  //used to keep track of errors
+float errorPenalty = 0.5f; //for every error, add this to mean time
+int startTime = 0; // time starts when the first click is captured
+int finishTime = 0; //records the time of the final click
+boolean userDone = false;
+
+HScrollbar hs1;
+float img1Pos;
+
+final int screenPPI = 72; //what is the DPI of the screen you are using 
+
+private class Target
+{
+  float x = 0;
+  float y = 0;
+  float rotation = 0;
+  float z = 0;
+}
+
+ArrayList<Target> targets = new ArrayList<Target>();
+
+float inchesToPixels(float inch)
+{
+  return inch*screenPPI;
+}
+
+void setup() {
+  size(800,800); 
+
+  hs1 = new HScrollbar(0, 16, width, 16, 16);
+
+  rectMode(CENTER);
+  textFont(createFont("Arial", inchesToPixels(.2f))); //sets the font to Arial that is .3" tall
+  textAlign(CENTER);
+
+  //don't change this! 
+  border = inchesToPixels(.2f); //padding of 0.2 inches
+
+  for (int i=0; i<trialCount; i++) //don't change this! 
+  {
+    Target t = new Target();
+    t.x = random(-width/2+border, width/2-border); //set a random x with some padding
+    t.y = random(-height/2+border, height/2-border); //set a random y with some padding
+    t.rotation = random(0, 360); //random rotation between 0 and 360
+    int j = (int)random(20);
+    t.z = ((j%20)+1)*inchesToPixels(.15f); //increasing size from .15 up to 3.0" 
+    targets.add(t);
+    println("created target with " + t.x + "," + t.y + "," + t.rotation + "," + t.z);
+  }
+
+  Collections.shuffle(targets); // randomize the order of the button; don't change this.
+}
+
+void draw() {
+
+  background(60); //background is dark grey
+  fill(200);
+  noStroke();
+
+  if (userDone)
+  {
+    text("User completed " + trialCount + " trials", width/2, inchesToPixels(.2f));
+    text("User had " + errorCount + " error(s)", width/2, inchesToPixels(.2f)*2);
+    text("User took " + (finishTime-startTime)/1000f/trialCount + " sec per target", width/2, inchesToPixels(.2f)*3);
+    text("User took " + ((finishTime-startTime)/1000f/trialCount+(errorCount*errorPenalty)) + " sec per target inc. penalty", width/2, inchesToPixels(.2f)*4);
+    return;
+  }
+
+  //===========DRAW TARGET SQUARE=================
+  pushMatrix();
+  translate(width/2, height/2); //center the drawing coordinates to the center of the screen
+
+  Target t = targets.get(trialIndex);
+
+  translate(t.x, t.y); //center the drawing coordinates to the center of the screen
+  translate(screenTransX, screenTransY); //center the drawing coordinates to the center of the screen
+
+  rotate(radians(t.rotation));
+
+  fill(255, 0, 0); //set color to semi translucent
+  rect(0, 0, t.z, t.z);
+
+  popMatrix();
+
+  //===========DRAW TARGETTING SQUARE=================
+  pushMatrix();
+  translate(width/2, height/2); //center the drawing coordinates to the center of the screen
+  rotate(radians(screenRotation));
+  //custom shifts:
+  //translate(screenTransX,screenTransY); //center the drawing coordinates to the center of the screen
+  fill(255, 128); //set color to semi translucent
+  rect(0, 0, screenZ, screenZ);
+  popMatrix();
+  
+  displayNums();
+  scaffoldControlLogic(); //you are going to want to replace this!
+  
+  text("Trial " + (trialIndex+1) + " of " +trialCount, width/2, inchesToPixels(.5f));
+
+  //===========SCROLLBAR================================
+ // background(255);
+  
+  // Get the position of the img1 scrollbar
+  // and convert to a value to display the img1 image 
+  img1Pos = hs1.getPos();
+  //fill(155);
+  //image(img1, width/2-img1.width/2 + img1Pos*1.5, 0);
+  
+  hs1.update();
+  hs1.display();
+  
+  stroke(0);
+  line(0, height/2, width, height/2);
+}
+
+void displayNums(){
+  //show the user where the square is
+/*  Target t = targets.get(trialIndex);
+  text(t.x+screenTransX+375, inchesToPixels(.5f), inchesToPixels(.4f));
+  text(t.y+screenTransY+375, inchesToPixels(.5f), inchesToPixels(.6f));
+  text(t.rotation, inchesToPixels(.5f), inchesToPixels(.8f));
+  text(t.z, inchesToPixels(.5f), inchesToPixels(1f));*/
+  text(img1Pos, inchesToPixels(.5f), inchesToPixels(.4f));
+
+  text(mouseX, width-inchesToPixels(.5f), inchesToPixels(.4f));
+  text(mouseY, width-inchesToPixels(.5f), inchesToPixels(.6f));
+}
+//my example design
+void scaffoldControlLogic()
+{
+/*
+  //upper left corner, rotate counterclockwise
+  text("CCW", inchesToPixels(.2f), inchesToPixels(.2f));
+  if (mousePressed && dist(0, 0, mouseX, mouseY)<inchesToPixels(.5f))
+    screenRotation--;
+
+  //upper right corner, rotate clockwise
+  text("CW", width-inchesToPixels(.2f), inchesToPixels(.2f));
+  if (mousePressed && dist(width, 0, mouseX, mouseY)<inchesToPixels(.5f))
+    screenRotation++;
+*/
+
+  Target t = targets.get(trialIndex);
+  t.z = inchesToPixels(((img1Pos/width)*3));
+  //lower left corner, decrease Z
+  text("-", inchesToPixels(.2f), height-inchesToPixels(.2f));
+  if (mousePressed && dist(0, height, mouseX, mouseY)<inchesToPixels(.5f))
+    screenZ-=inchesToPixels(.02f);
+
+  //lower right corner, increase Z
+  text("+", width-inchesToPixels(.2f), height-inchesToPixels(.2f));
+  if (mousePressed && dist(width, height, mouseX, mouseY)<inchesToPixels(.5f))
+    screenZ+=inchesToPixels(.02f);
+
+  //left middle, move left
+  text("left", inchesToPixels(.2f), height/2);
+  if (mousePressed && dist(0, height/2, mouseX, mouseY)<inchesToPixels(.5f))
+    screenTransX-=inchesToPixels(.02f);
+  
+  text("-10", inchesToPixels(.2f), height/2 - 30);
+  if (mousePressed && dist(0, height/2 - 30, mouseX, mouseY)<inchesToPixels(.5f))
+    screenTransX-=10;
+    
+  text("-100", inchesToPixels(.2f), height/2 - 60);
+  if (mousePressed && dist(0, height/2 - 60, mouseX, mouseY)<inchesToPixels(.5f))
+    screenTransX-=100;
+
+  text("right", width-inchesToPixels(.2f), height/2);
+  if (mousePressed && dist(width, height/2, mouseX, mouseY)<inchesToPixels(.5f))
+    screenTransX+=inchesToPixels(.02f);
+/* 
+  text("up", width/2, inchesToPixels(.2f));
+  if (mousePressed && dist(width/2, 0, mouseX, mouseY)<inchesToPixels(.5f))
+    screenTransY-=inchesToPixels(.02f);
+*/
+  text("down", width/2, height-inchesToPixels(.2f));
+  if (mousePressed && dist(width/2, height, mouseX, mouseY)<inchesToPixels(.5f))
+    screenTransY+=inchesToPixels(.02f);
+}
+
+
+void mousePressed()
+{
+    if (startTime == 0) //start time on the instant of the first user click
+    {
+      startTime = millis();
+      println("time started!");
+    }
+}
+
+
+void mouseReleased()
+{
+  //check to see if user clicked middle of screen
+  if (dist(width/2, height/2, mouseX, mouseY)<inchesToPixels(.5f))
+  {
+    if (userDone==false && !checkForSuccess())
+      errorCount++;
+
+    //and move on to next trial
+    trialIndex++;
+
+    screenTransX = 0;
+    screenTransY = 0;
+
+    if (trialIndex==trialCount && userDone==false)
+    {
+      userDone = true;
+      finishTime = millis();
+    }
+  }
+}
+
+
+public boolean checkForSuccess()
+{
+  Target t = targets.get(trialIndex);  
+  boolean closeDist = dist(t.x,t.y,-screenTransX,-screenTransY)<inchesToPixels(.05f); //has to be within .1"
+  boolean closeRotation = calculateDifferenceBetweenAngles(t.rotation,screenRotation)<=5;
+  boolean closeZ = abs(t.z - screenZ)<inchesToPixels(.05f); //has to be within .1"  
+  
+  println("Close Enough Distance: " + closeDist);
+  println("Close Enough Rotation: " + closeRotation + "(dist="+calculateDifferenceBetweenAngles(t.rotation,screenRotation)+")");
+  println("Close Enough Z: " + closeZ);
+  
+  return closeDist && closeRotation && closeZ;  
+}
+
+
+double calculateDifferenceBetweenAngles(float a1, float a2)
+  {
+     double diff=abs(a1-a2);
+      diff%=90;
+      if (diff>45)
+        return 90-diff;
+      else
+        return diff;
+ }
+
+class HScrollbar {
+  int swidth, sheight;    // width and height of bar
+  float xpos, ypos;       // x and y position of bar
+  float spos, newspos;    // x position of slider
+  float sposMin, sposMax; // max and min values of slider
+  int loose;              // how loose/heavy
+  boolean over;           // is the mouse over the slider?
+  boolean locked;
+  float ratio;
+
+  HScrollbar (float xp, float yp, int sw, int sh, int l) {
+    swidth = sw;
+    sheight = sh;
+    int widthtoheight = sw - sh;
+    ratio = (float)sw / (float)widthtoheight;
+    xpos = xp;
+    ypos = yp-sheight/2;
+    spos = xpos + swidth/2 - sheight/2;
+    newspos = spos;
+    sposMin = xpos;
+    sposMax = xpos + swidth - sheight;
+    loose = l;
+  }
+
+  void update() {
+    if (overEvent()) {
+      over = true;
+    } else {
+      over = false;
+    }
+    if (mousePressed && over) {
+      locked = true;
+    }
+    if (!mousePressed) {
+      locked = false;
+    }
+    if (locked) {
+      newspos = constrain(mouseX-sheight/2, sposMin, sposMax);
+    }
+    if (abs(newspos - spos) > 1) {
+      spos = spos + (newspos-spos)/loose;
+    }
+  }
+
+  float constrain(float val, float minv, float maxv) {
+    return min(max(val, minv), maxv);
+  }
+
+  boolean overEvent() {
+    if (mouseX > xpos && mouseX < xpos+swidth &&
+       mouseY > ypos && mouseY < ypos+sheight) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void display() {
+    noStroke();
+    fill(204);
+    //rect(xpos, ypos, swidth, sheight);
+    if (over || locked) {
+      fill(0, 0, 0);
+    } else {
+      fill(102, 102, 102);
+    }
+    rect(spos, ypos, sheight, sheight);
+  }
+
+  float getPos() {
+    // Convert spos to be values between
+    // 0 and the total width of the scrollbar
+    return spos * ratio;
+  }
+}
